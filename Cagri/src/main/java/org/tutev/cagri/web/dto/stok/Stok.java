884f7dfe2c70f0c0bb6f76eeec048ca21606724b
serdal.guzel <guzelserdal@gmail.com>
2014-11-28 09:41:16 +0200

    Stok ve Stok Türlerinden düzenlemeler

--- Cagri/src/main/java/org/tutev/cagri/web/dto/stok/Stok.java
+++ Cagri/src/main/java/org/tutev/cagri/web/dto/stok/Stok.java
@@ -1,22 +1,47 @@
 package org.tutev.cagri.web.dto.stok;
 
 import java.io.Serializable;
-
 import java.math.BigDecimal;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
+
+import org.tutev.cagri.web.dto.firma.Firmalar;
 
 @Entity
 @Table(name = "STOK")
+@SequenceGenerator(name = "STOK_ID_SEQ")
 public class Stok implements Serializable {
 	private long stokId;
+
 	private String stokAdi;
 	private String stokAciklamasi;
 	private StokTurleri stokTurleri;
 	private StokBirimleri stokBirimleri;
+	private Firmalar firmalar;
 	private BigDecimal fiyati;
 
+	public Stok(String stokAdi, String stokAciklamasi, StokTurleri stokTurleri,
+			StokBirimleri stokBirimleri, Firmalar firmalar, BigDecimal fiyati) {
+		this.stokAdi = stokAdi;
+		this.stokAciklamasi = stokAciklamasi;
+		this.stokTurleri = stokTurleri;
+		this.stokBirimleri = stokBirimleri;
+		this.firmalar = firmalar;
+		this.fiyati = fiyati;
+	}
+
+	@Id
+	@Column(name = "STOK_ID")
+	@GeneratedValue(generator = "STOK_ID_SEQ", strategy = GenerationType.SEQUENCE)
 	public long getStokId() {
 		return stokId;
 	}
@@ -25,6 +50,7 @@ public class Stok implements Serializable {
 		this.stokId = stokId;
 	}
 
+	@Column(name = "STOK_ADI", length = 100)
 	public String getStokAdi() {
 		return stokAdi;
 	}
@@ -33,6 +59,7 @@ public class Stok implements Serializable {
 		this.stokAdi = stokAdi;
 	}
 
+	@Column(name = "STOK_ACIKLAMASI", length = 400)
 	public String getStokAciklamasi() {
 		return stokAciklamasi;
 	}
@@ -41,6 +68,8 @@ public class Stok implements Serializable {
 		this.stokAciklamasi = stokAciklamasi;
 	}
 
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "STOK_TUR_ID")
 	public StokTurleri getStokTurleri() {
 		return stokTurleri;
 	}
@@ -49,6 +78,8 @@ public class Stok implements Serializable {
 		this.stokTurleri = stokTurleri;
 	}
 
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "STOK_BIRIMLERI_ID")
 	public StokBirimleri getStokBirimleri() {
 		return stokBirimleri;
 	}
@@ -57,12 +88,22 @@ public class Stok implements Serializable {
 		this.stokBirimleri = stokBirimleri;
 	}
 
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "FIRMA_ID")
+	public Firmalar getFirmalar() {
+		return firmalar;
+	}
+
+	public void setFirmalar(Firmalar firmalar) {
+		this.firmalar = firmalar;
+	}
+
 	public BigDecimal getFiyati() {
 		return fiyati;
 	}
 
+	@Column(name = "FIYATI", precision = 10, scale = 2)
 	public void setFiyati(BigDecimal fiyati) {
 		this.fiyati = fiyati;
 	}
-
 }
--- Cagri/src/main/java/org/tutev/cagri/web/dto/stok/StokTurleri.java
+++ Cagri/src/main/java/org/tutev/cagri/web/dto/stok/StokTurleri.java
@@ -2,14 +2,54 @@ package org.tutev.cagri.web.dto.stok;
 
 import java.io.Serializable;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "STOK_TURLERI")
-@SequenceGenerator(name="STOK_TURLERI_ID_SEQ")
+@SequenceGenerator(name = "STOK_TUR_ID_SEQ")
 public class StokTurleri implements Serializable {
 
+	private long stokTurId;
+	private String stokTurAdi;
+	private Boolean aktif;
 
+	public StokTurleri(String stokTurAdi, Boolean aktif) {
+		this.stokTurAdi = stokTurAdi;
+		this.aktif = aktif;
+	}
+
+	@Id
+	@Column(name = "STOK_TUR_ID")
+	@GeneratedValue(generator = "STOK_TUR_ID_SEQ", strategy = GenerationType.SEQUENCE)
+	public long getStokTurId() {
+		return stokTurId;
+	}
+
+	public void setStokTurId(long stokTurId) {
+		this.stokTurId = stokTurId;
+	}
+
+	@Column(name = "STOK_TUR_ADI", length = 100)
+	public String getStokTurAdi() {
+		return stokTurAdi;
+	}
+
+	public void setStokTurAdi(String stokTurAdi) {
+		this.stokTurAdi = stokTurAdi;
+	}
+
+	@Column(name = "AKTIF", length = 1)
+	public Boolean getAktif() {
+		return aktif;
+	}
+
+	public void setAktif(Boolean aktif) {
+		this.aktif = aktif;
+	}
 }
