6babf6ed27c0f7b5acecf385316e853bdc479d61
hideux <hideux@live.com>
2014-12-14 18:06:38 +0200

    menu added to CGR01 page

--- Cagri/src/main/java/org/tutev/cagri/web/controller/uygulama/LoginController.java
+++ Cagri/src/main/java/org/tutev/cagri/web/controller/uygulama/LoginController.java
@@ -1,5 +1,7 @@
 package org.tutev.cagri.web.controller.uygulama;
 
+import java.io.IOException;
+
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 import javax.servlet.http.HttpSession;
@@ -45,7 +47,7 @@ public class LoginController {
 		this.password = password;
 	}
 
-	public String login() {
+	public String login() throws IOException {
 		kullanici = loginService.getKullaniciByUsernamePassword(username, password);
 		if (kullanici == null) {
 			FacesContext.getCurrentInstance().addMessage(null,
@@ -54,6 +56,7 @@ public class LoginController {
 		} else {
 			HttpSession session = (HttpSession) FacesContext.getCurrentInstance().getExternalContext().getSession(false);
 			session.setAttribute("kullanici", kullanici);
+			FacesContext.getCurrentInstance().getExternalContext().redirect("secure/Cagri/CGR01.xhtml");
 			return "index";
 		}
 	}
