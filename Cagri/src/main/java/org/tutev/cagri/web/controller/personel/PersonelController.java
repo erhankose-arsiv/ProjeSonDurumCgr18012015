2e8e38dfad0d07d7dbfa62a82acb90b50ba2f151
Kuvvat <Kuvvat@Comp>
2014-12-21 16:14:19 +0200

    PersonelController PersonelService Per01.xhtml degiştirildi

--- Cagri/src/main/java/org/tutev/cagri/web/controller/personel/PersonelController.java
+++ Cagri/src/main/java/org/tutev/cagri/web/controller/personel/PersonelController.java
@@ -2,13 +2,23 @@ package org.tutev.cagri.web.controller.personel;
 
 import java.io.Serializable;
 import java.util.List;
+import java.util.Map;
 
 import javax.annotation.PostConstruct;
+import javax.faces.context.FacesContext;
 
+import org.primefaces.context.RequestContext;
+import org.primefaces.model.LazyDataModel;
+import org.primefaces.model.SortOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
+import org.tutev.cagri.web.base.OrderData;
+import org.tutev.cagri.web.base.OrderType;
+import org.tutev.cagri.web.base.QueryResults;
+import org.tutev.cagri.web.controller.MessageController;
 import org.tutev.cagri.web.dto.personel.Personel;
+import org.tutev.cagri.web.service.GenericService;
 import org.tutev.cagri.web.service.personelService.PersonelService;
 
 @Controller("personelController")
@@ -19,17 +29,66 @@ public class PersonelController implements Serializable {
 	Long tc;
 	
 	@Autowired
-	private PersonelService personelService;
+	private transient PersonelService personelService;
+	@Autowired
+	private transient GenericService genericService;
+	@Autowired
+	private transient MessageController messageController;
+
 	
+	public Personel getPersonel() {
+		if(this.personel==null){
+			this.personel=new Personel();
+			
+		}
+		return personel;
+	}
+	public void setPersonel(Personel personel) {
+		this.personel = personel;
+	}
+
 	Personel  personel;
 	List<Personel> personelList;
 	
 	@PostConstruct
 	public void init() {		
-		personelList=personelService.getAll();
+		FacesContext.getCurrentInstance().getExternalContext().getSession(true);
+		listele();
+		personel = new Personel();
 	}
 	
-	
+	private LazyDataModel<Personel> lazyModel;
+	public LazyDataModel<Personel> getLazyModel() {
+		return lazyModel;
+	}
+	public void listele() {
+		this.lazyModel= new  LazyDataModel<Personel>() {
+			private static final long serialVersionUID = 1L;
+			
+			private List<Personel> liste;
+			@SuppressWarnings("unchecked")
+			@Override
+			public List<Personel> load(int firstRecord, int pageSize, String sortField,SortOrder sortOrder, Map<String, String> filters) {
+				
+				OrderData orderData = null;
+				if (sortField != null) {
+					orderData = new OrderData(sortField, sortOrder.equals(SortOrder.ASCENDING) ? OrderType.ASC : OrderType.DESC);
+				}
+				else {
+					orderData = new OrderData("id", OrderType.ASC);
+				}
+				
+				QueryResults qr = personelService.getPersonelListesi(filters, orderData, firstRecord, pageSize);
+				lazyModel.setRowCount(qr.getRowCount());
+				liste = qr.getListe();
+				return liste;
+
+			}
+
+		
+		};
+	}
+
 	public Long getTc() {
 		return tc;
 	}
@@ -48,7 +107,38 @@ public class PersonelController implements Serializable {
 	public void setPersonelList(List<Personel> personelList) {
 		this.personelList = personelList;
 	}
-
+	
+	public void onRowSelect() {
+		
+	}
+	
+	public void yeni() {
+		personel=null;
+		RequestContext.getCurrentInstance().update(":personelForm:dlgPersonel");
+	}
+	
+	public void clearFilters() {
+		
+	}
+	
+	public void save() {
+		if(personel.getId()==null){
+			personelService.save(personel);
+			messageController.kaydetmeBasarili();
+		}else{
+			personelService.update(personel);
+			messageController.guncellemeBasarili();
+		}
+	
+		personel = null;
+		listele();
+	
+	}
+	
+	public void delete() {
+		personelService.delete(personel);
+		listele();
+	}
+}
 
 	
-}
--- Cagri/src/main/java/org/tutev/cagri/web/service/personelService/PersonelService.java
+++ Cagri/src/main/java/org/tutev/cagri/web/service/personelService/PersonelService.java
@@ -1,12 +1,19 @@
 package org.tutev.cagri.web.service.personelService;
 
 import java.util.List;
+import java.util.Map;
 
 import org.apache.log4j.Logger;
+import org.hibernate.Criteria;
+import org.hibernate.criterion.MatchMode;
+import org.hibernate.criterion.Restrictions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.tutev.cagri.web.base.BaseDao;
+import org.tutev.cagri.web.base.OrderData;
+import org.tutev.cagri.web.base.QueryResults;
+import org.tutev.cagri.web.dto.Sabit;
 import org.tutev.cagri.web.dto.cagri.Cagri;
 import org.tutev.cagri.web.dto.personel.Personel;
 import org.tutev.cagri.web.service.CagriService;
@@ -19,7 +26,7 @@ Logger LOGGER =Logger.getLogger(PersonelService.class);
 	@Autowired
 	private transient BaseDao baseDao;
 	
-	private boolean save(Personel personel) {
+	public boolean save(Personel personel) {
 		try{
 			if(personel.getTc() !=null){
 				baseDao.save(personel);
@@ -41,5 +48,32 @@ Logger LOGGER =Logger.getLogger(PersonelService.class);
 			return null;
 		}
 	}
+	public QueryResults getPersonelListesi(Map<String, String> filters,OrderData orderData, int firstRecord, int pageSize) {
+		Criteria criteria =baseDao.getSession().createCriteria(Cagri.class);
+		if(filters.get("tc")!=null){
+			criteria.add(Restrictions.ilike("tc", filters.get("tc"),MatchMode.ANYWHERE));
+		}
+		return baseDao.getAll(criteria, orderData, firstRecord, pageSize);
+	}
 
+	public void update(Personel personel) {
+		try {
+			if (personel.getId() != null) {
+				baseDao.update(personel);
+			}
+		} catch (Exception e) {
+			LOGGER.error(e);
+		}
+
+	}
+
+	public boolean delete(Personel personel) {
+		try {
+			baseDao.delete(personel);
+			return true;
+		} catch (Exception e) {
+			LOGGER.error(e);
+			return false;
+		}
+	}
 }
